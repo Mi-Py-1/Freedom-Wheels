f124cd1e40e320e4e8e3db01cae81dcd6fe074f8
Michael Pymer <mikepymer@hotmail.co.uk>
2025-01-22 13:01:08 +0000

    updated profile

--- my_app/models.py
+++ my_app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 # import the built-in User model that you want to extend.
 from django.contrib.auth.models import User
-
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
@@ -25,11 +25,14 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+@receiver(post_save, sender=User)
 # create new profile upon creating new user
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
         user_profile.save()
+        user_profile.follows.add(instance.profile)
+        user_profile.save()
 
-# Create a Profile for each new user.
-post_save.connect(create_profile, sender=User)
+
+

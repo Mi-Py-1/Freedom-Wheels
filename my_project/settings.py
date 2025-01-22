bd1ac2f44f9129e201a0bdd6a5dce956d426ff23
Michael Pymer <mikepymer@hotmail.co.uk>
2025-01-22 10:15:00 +0000

    changed debug to false

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-txt@d6lk+=4^gq*_9mk=%k+573&9(e#f%zv07p_3sjsa^!wxq!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost',]
+ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
 
 
 # Application definition

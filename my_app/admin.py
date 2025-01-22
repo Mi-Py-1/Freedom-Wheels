3ba9efdc58874bf2c4f9b4d1cc121ac12568ff27
Michael Pymer <mikepymer@hotmail.co.uk>
2025-01-22 12:08:34 +0000

    Added:user profile and tested

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from .models import Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Only display the "username" field
+    fields = ["username", "email", "first_name", "last_name",]
+
 
 # Register your models here.
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Profile)
--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+# import the built-in User model that you want to extend.
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    # a OneToOneField object called user, representing the profile’s connection to the user. profile gets deleted if user does.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #  a ManyToManyField object with the field name follows, which can hold connections to other user profiles.
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        # so that your users can follow someone without them following back.
+        symmetrical=False,
+        # users don’t need to follow anyone.
+        blank=True
+    )
